4d7318e7da2ee6e310306b1903f2b1177aa677a7
neeks <neeraj.sejwal@gmail.com>
2017-10-06 15:23:03 +0530

    -add padding to cells/labels using attributed string paragraph styles

--- ios-labels-matrix-view-swift/NALLabelsMatrix/NALLabelsMatrix.swift
+++ ios-labels-matrix-view-swift/NALLabelsMatrix/NALLabelsMatrix.swift
@@ -15,6 +15,9 @@ class NALLabelsMatrixView : UIView {
     var numRows:Int=0
     var dy:Int=0
     
+    public var headerColour:UIColor = .lightGray
+    public var borderColour:UIColor = .gray
+    
     init(frame:CGRect, columns:Array<Int>) {
         columnWidths = columns
         super.init(frame: frame);
@@ -31,26 +34,56 @@ class NALLabelsMatrixView : UIView {
         let rowHeight:Int = 30
         var dx:Int = 0
         
-        
+        //CREATE THE ITEMS/COLUMNS OF THE ROW
         for i in 0..<record.count {
             let colWidth:Int = self.columnWidths[i]
             let cellRect:CGRect = CGRect.init(x: dx, y: self.dy, width: colWidth, height: rowHeight)
             
+            //ADJUST X FOR BORDER OVERLAPPING BETWEEN COLUMNS
             dx += colWidth - 1
             
+            //CREATE THE LABEL TO BE USED AS CELL
             let column:UILabel = UILabel.init(frame: cellRect)
-            column.layer.borderColor = UIColor.darkGray.cgColor
+            column.layer.borderColor = self.borderColour.cgColor
             column.layer.borderWidth = 1.0
             column.font = UIFont.init(name: "Helvetica", size: 12.0)
             
-            column.text = record[i]
+            
+            //SET LEFT RIGHT MARGINS & ALIGNMENT FOR THE LABEL
+            let paragraphStyle:NSMutableParagraphStyle = NSMutableParagraphStyle.init()
+            paragraphStyle.alignment = .natural
+            paragraphStyle.headIndent = 10.0
+            paragraphStyle.firstLineHeadIndent = 10.0
+            paragraphStyle.tailIndent = -10.0
+            
+            //SPECIAL TREATMENT FOR THE FIRST ROW
+            if(self.numRows == 0){
+                paragraphStyle.alignment = .center
+                column.backgroundColor = self.headerColour
+            }
+            let attrText:NSAttributedString = NSAttributedString.init(string: record[i], attributes: [NSParagraphStyleAttributeName: paragraphStyle])
+            
+            
+            //USED TO FIND HEIGHT OF LONGEST LABEL
+            //
+            //MAKE THE LABEL WIDTH SAME AS COLUMN'S WIDTH
+            //
+            //USED FOR SETTING THE NEXT COLUMN X POSITION
+            //
+            
+            column.attributedText = attrText
             self.addSubview(column)
         }
         
-        self.numRows += 1
+        //MAKE ALL THE LABELS OF SAME HEIGHT AND THEN ADD TO VIEW
+        //
+        
+        //ADJUST Y FOR BORDER OVERLAPPING BETWEEN ROWS
         self.dy += rowHeight - 1
         
+        self.numRows += 1
         
         return;
     }
 }
+
